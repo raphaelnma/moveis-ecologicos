f8987f0359fccce3675053c37742590448369bdf
Raphael Mantilha <raphael.mantilha@gmail.com>
2019-01-14 11:37:38 -0200

    Movendo principal.js

--- principal.js
+++
@@ -1,9 +0,0 @@
-setInterval(trocaBanner, 1000);
-var banners =
-["Os melhores do Brasil!", "Qualidade e preço baixo!"];
-var bannerAtual = 0;
-function trocaBanner() {
-bannerAtual = (bannerAtual + 1) % 2;
-document.querySelector(’h2#mensagem’).textContent =
-banners[bannerAtual];
-}
